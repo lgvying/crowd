f6605dda1f63987075bd6fbdd89ee9d0471bbd07
lgvying <15236603373@163.com>
2021-06-30 16:58:05 +0800

    删除Addressing 的导入

--- src/test/java/com/zut/boot/CrowdApplicationTests.java
+++ src/test/java/com/zut/boot/CrowdApplicationTests.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import javax.xml.ws.soap.Addressing;
 import java.util.List;
 
 @SpringBootTest
